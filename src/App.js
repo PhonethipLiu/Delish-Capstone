b99c4f3581238821414351110a6035cb5725f869
Phonethip <phonethip.liu@gmail.com>
2018-06-14 18:32:13 -0500

    added new ReadMe and more npm installs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/Delish-logo-01.svg';
 import './App.css';
-
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Recipe Box</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
